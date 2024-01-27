0d16bcebe6a3243e72bd996ad020ac20c4182d2a
VILA Loris <loris.vila@sncf.fr>
2024-01-27 18:39:23 +0100

    feat : Added a still in maintenance page

---
+++ src/app/pages/templates/still-in-dev-page/still-in-dev-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-still-in-dev-page',
+  templateUrl: './still-in-dev-page.component.html',
+  styleUrls: ['./still-in-dev-page.component.css']
+})
+export class StillInDevPageComponent {
+
+}
