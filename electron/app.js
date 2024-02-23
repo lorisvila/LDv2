c097120344b33952c869ac23ea5c453f738c4887
VILA Loris <loris.vila@sncf.fr>
2024-02-23 11:52:42 +0100

    refactor : initial commit of the electron app

---
+++ electron/app.js
@@ -0,0 +1,31 @@
+// Electron setup / config
+
+const {
+  app,
+  BrowserWindow
+} = require('electron')
+const path = require('path')
+let appWindow
+
+function createWindow() {
+  appWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: path.join(__dirname, "dist", "ldV2_Angular", "assets", "sncf-logo.jpg"),
+    autoHideMenuBar: true,
+  })
+
+  //appWindow.webContents.openDevTools()
+
+  appWindow.maximize()
+
+  appWindow.loadFile("dist/ldV2_Angular/index.html");
+
+  appWindow.on('closed', function () {
+    appWindow = null
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
